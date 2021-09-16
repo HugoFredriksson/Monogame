67a15b95c3f35e67585373d414fe62643d5b0d74
HugoFredriksson <89245482+HugoFredriksson@users.noreply.github.com>
2021-09-16 20:04:58 +0200

    Create Game1.cs

---
+++ Game1.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace spelsak
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D marko;
+
+        Vector2 markoPos = new Vector2(100, 300);
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+            
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            
+
+            marko = Content.Load<Texture2D>("marko");
+
+            
+
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Right))
+            markoPos.X++;
+            if (kstate.IsKeyDown(Keys.Left))
+            markoPos.X--;
+            base.Update(gameTime);
+            
+            if (kstate.IsKeyDown(Keys.Up))
+            markoPos.Y--;
+            if (kstate.IsKeyDown(Keys.Down))
+            markoPos.Y++;
+            base.Update(gameTime);
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(marko, markoPos, Color.White);
+            _spriteBatch.End();
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
